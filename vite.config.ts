cd1011b056bb73cc392e88e7afb6c955a736b388
ridaelgharass-spec <rida.elgharass@gmail.com>
2026-03-20 17:51:58 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+//  base: '/1995/',
   base: '/1995/',
   plugins: [react(), tailwindcss()],
   build: {
